5c096d0400f52104de5220821fc451b155fe294f
GouravSingh56 <138119422+GouravSingh56@users.noreply.github.com>
2025-11-21 02:08:20 +0530

    Add files via upload

---
+++ 03_conversionOperaton.js
@@ -0,0 +1,44 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Singh"
+
+
+//  console.log(typeof score);
+// conversion from number to string
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1 , false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Tom"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false , "Gourav" => true
+// null => false
+// undefined => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
